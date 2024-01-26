8d3f5a8852d2212b0371c6f2635113d093a35fe1
demirhany <demirhanyilal0@gmail.com>
2024-01-27 01:24:23 +0300

    GameLogic is updated

--- ggj2024/Assets/Scripts/GameLogic.cs
+++ ggj2024/Assets/Scripts/GameLogic.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class GameLogic : MonoBehaviour
 {
     // Start is called before the first frame update
+    public List<Character> judges;
+    public Character characters;
+    public RandomCharacterSelector characterSelector;
+    public List<Character> selectedCharacter;
     void Start()
     {
+        selectedCharacter = new List<Character>();
+        characters = GameObject.FindGameObjectWithTag("Character").GetComponent<Character>();
+        characterSelector = GameObject.FindGameObjectWithTag("RandomCharacterSelector").GetComponent<RandomCharacterSelector>();
         
+        for(int i = 0; i < 3; i++)
+        {
+            selectedCharacter.Add(characters.characters[characterSelector.SelectJuris()[i]])
+        }
     }
 
     // Update is called once per frame
