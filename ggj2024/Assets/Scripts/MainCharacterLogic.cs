46e16482771b98d03edf473ff53617b16bc7b108
demirhany <demirhanyilal0@gmail.com>
2024-01-27 00:37:24 +0300

    MainCharacterLogic is updated

--- ggj2024/Assets/Scripts/MainCharacterLogic.cs
+++ ggj2024/Assets/Scripts/MainCharacterLogic.cs
@@ -1,18 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainCharacterLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isButtonClicked;
+    public List<object>cards;
+    private List<object> selectedFiveCards;
+    private void Awake()
+    {
+        cards = new List<object>(); // bu kartlari, kartlari array olarak donduren bir fonksiyonu kullanarak cekecegiz
+        //card = GameObject.FindGameObjectWithTag("Card").GetComponent<CardScript>();
+        //cards = card.GetCard(); //GetCard() bir object array dondurmeli
+
+        for(int i = 0; i < 30; i++)
+        {
+            cards.Add(("deneme" + i, i));
+        }
+        selectedFiveCards = SelectRandomFiveCards();
+        foreach (var item in selectedFiveCards)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeIsClicked()
     {
-        
+        isButtonClicked = true;
+    }
+
+    public List<object> SelectRandomFiveCards()
+    {
+        List<int> cardId = new List<int>();
+        for (int j = 0; j < 30; j++)
+        {
+            cardId.Add(j);
+        }
+        List<object> selectedCards = new List<object>();
+        for(int j = 0; j < 5; j++)
+        {
+            int value = Random.Range(0, cardId.Count);
+            selectedCards.Add(cards[cardId[value]]);
+            cardId.RemoveAt(value);
+        }
+        return selectedCards;
     }
 }
