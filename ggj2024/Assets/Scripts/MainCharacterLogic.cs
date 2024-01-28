ff5cc805285fb896d0c73f73b2bd3e975de591ea
demirhany <demirhanyilal0@gmail.com>
2024-01-28 14:44:19 +0300

    Card Images Added

--- ggj2024/Assets/Scripts/MainCharacterLogic.cs
+++ ggj2024/Assets/Scripts/MainCharacterLogic.cs
@@ -13,14 +13,15 @@ public class Card
     public int CharacterId;
     public List<int> Dislikes;
     public bool isOnclick;
-
-    public Card(int id, string name, int characterId, List<int> dislikes)
+    public string ImagePath;
+    public Card(int id, string name, int characterId, List<int> dislikes, string imagePath)
     {
         this.Id = id;
         this.name = name;
         this.CharacterId = characterId;
         isOnclick = false;
         Dislikes = dislikes;
+        ImagePath = imagePath;
     }
     public Card()
     {
@@ -94,18 +95,23 @@ public class MainCharacterLogic : MonoBehaviour
         //card = GameObject.FindGameObjectWithTag("Card").GetComponent<CardScript>();
         //cards = card.GetCard();
         ////GetCard() bir object array dondurmeli
-        List<string> cardTexts = new List<string>();
+        List<string> cardImagesPath = new List<string>();
         for (int i = 0; i < 5; i++)
         {
             selectedFiveCards.Add(SelectRandomOneCard());
-            cardTexts.Add(selectedFiveCards[i].name);
+            cardImagesPath.Add(selectedFiveCards[i].ImagePath);
         }
-        card1.GetComponentInChildren<TextMeshPro>().text = cardTexts[0];
-        card2.GetComponentInChildren<TextMeshPro>().text = cardTexts[1];
-        card3.GetComponentInChildren<TextMeshPro>().text = cardTexts[2];
-        card4.GetComponentInChildren<TextMeshPro>().text = cardTexts[3];
-        card5.GetComponentInChildren<TextMeshPro>().text = cardTexts[4];
-
+        GameObject.FindGameObjectWithTag("image1").GetComponent<Image>().sprite = Resources.Load<Sprite>(selectedFiveCards[0].ImagePath);
+        GameObject.FindGameObjectWithTag("image2").GetComponent<Image>().sprite = Resources.Load<Sprite>(selectedFiveCards[1].ImagePath);
+        GameObject.FindGameObjectWithTag("image3").GetComponent<Image>().sprite = Resources.Load<Sprite>(selectedFiveCards[2].ImagePath);
+        GameObject.FindGameObjectWithTag("image4").GetComponent<Image>().sprite = Resources.Load<Sprite>(selectedFiveCards[3].ImagePath);
+        GameObject.FindGameObjectWithTag("image5").GetComponent<Image>().sprite = Resources.Load<Sprite>(selectedFiveCards[4].ImagePath);
+        /*card1.GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>(cardImagesPath[0]);
+        card2.GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>(cardImagesPath[1]);
+        card3.GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>(cardImagesPath[2]);
+        card4.GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>(cardImagesPath[3]);
+        card5.GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>(cardImagesPath[4]);
+        */
         SelectThreeRandomJudges();
         
         updateJudgeSprites(ThreeRaandomJudges[0].Id,judge1);
@@ -624,36 +630,36 @@ public class MainCharacterLogic : MonoBehaviour
         List<int> SekulerDislikes = new List<int>(); SekulerDislikes.Add(5); SekulerDislikes.Add(3); SekulerDislikes.Add(9);
         List<int> DunyaBariscisiDislikes = new List<int>(); DunyaBariscisiDislikes.Add(5); DunyaBariscisiDislikes.Add(3); DunyaBariscisiDislikes.Add(9);
         List<int> SatanistDislikes = new List<int>(); SatanistDislikes.Add(0); SatanistDislikes.Add(1); SatanistDislikes.Add(2); SatanistDislikes.Add(3); SatanistDislikes.Add(5); SatanistDislikes.Add(6); SatanistDislikes.Add(7); SatanistDislikes.Add(8);
-        cards.Add(new Card(0, "Feminist 0",0, FeministDislikes));
-        cards.Add(new Card(1, "Feminist 1", 0, FeministDislikes));
-        cards.Add(new Card(2, "Feminist 2", 0, FeministDislikes));
-        cards.Add(new Card(3, "Siyasal Islamci 3",1, SiyasalIslamciDislikes));
-        cards.Add(new Card(4, "Siyasal Islamci 4", 1, SiyasalIslamciDislikes));
-        cards.Add(new Card(5, "Siyasal Islamci 5", 1, SiyasalIslamciDislikes));
-        cards.Add(new Card(6, "Doga Sever 6",2, DogaSeverDislikes));
-        cards.Add(new Card(7, "Doga Sever 7", 2, DogaSeverDislikes));
-        cards.Add(new Card(8, "Doga Sever 8", 2, DogaSeverDislikes));
-        cards.Add(new Card(9, "Milliyetci 9",3, MilliyetciDislikes));
-        cards.Add(new Card(10, "Milliyetci 10", 3, MilliyetciDislikes));
-        cards.Add(new Card(11, "Milliyetci 11", 3, MilliyetciDislikes));
-        cards.Add(new Card(12, "Geek 12",4, GeekDislikes));
-        cards.Add(new Card(13, "Geek 13", 4, GeekDislikes));
-        cards.Add(new Card(14, "Geek 14", 4, GeekDislikes));
-        cards.Add(new Card(15, "Asiri Sanatci 15",5, AsiriSanatciDislikes));
-        cards.Add(new Card(16, "Asiri Sanatci 16", 5, AsiriSanatciDislikes));
-        cards.Add(new Card(17, "Asiri Sanatci 17", 5, AsiriSanatciDislikes));
-        cards.Add(new Card(18, "Insel 18",6, InselDislikes));
-        cards.Add(new Card(19, "Insel 19", 6, InselDislikes));
-        cards.Add(new Card(20, "Insel 20", 6, InselDislikes));
-        cards.Add(new Card(21, "Sekuler 21",7, SekulerDislikes));
-        cards.Add(new Card(22, "Sekuler 22", 7, SekulerDislikes));
-        cards.Add(new Card(23, "Sekuler 23", 7, SekulerDislikes));
-        cards.Add(new Card(24, "Dunya Bariscilari 24",8, DunyaBariscisiDislikes));
-        cards.Add(new Card(25, "Dunya Bariscilari 25", 8, DunyaBariscisiDislikes));
-        cards.Add(new Card(26, "Dunya Bariscilari 26", 8, DunyaBariscisiDislikes));
-        cards.Add(new Card(27, "Satanist 27",9, SatanistDislikes));
-        cards.Add(new Card(28, "Satanist 28", 9, SatanistDislikes));
-        cards.Add(new Card(29, "Satanist 29", 9, SatanistDislikes));
+        cards.Add(new Card(0, "Feminist 0",0, FeministDislikes, "Images/feminist"));
+        cards.Add(new Card(1, "Feminist 1", 0, FeministDislikes, "Images/feminist"));
+        cards.Add(new Card(2, "Feminist 2", 0, FeministDislikes, "Images/feminist"));
+        cards.Add(new Card(3, "Siyasal Islamci 3",1, SiyasalIslamciDislikes, "Images/siyasal_islam"));
+        cards.Add(new Card(4, "Siyasal Islamci 4", 1, SiyasalIslamciDislikes, "Images/siyasal_islam"));
+        cards.Add(new Card(5, "Siyasal Islamci 5", 1, SiyasalIslamciDislikes, "Images/siyasal_islam"));
+        cards.Add(new Card(6, "Doga Sever 6",2, DogaSeverDislikes, "Images/doga_sever"));
+        cards.Add(new Card(7, "Doga Sever 7", 2, DogaSeverDislikes, "Images/doga_sever"));
+        cards.Add(new Card(8, "Doga Sever 8", 2, DogaSeverDislikes, "Images/doga_sever"));
+        cards.Add(new Card(9, "Milliyetci 9",3, MilliyetciDislikes, "Images/milliyetci"));
+        cards.Add(new Card(10, "Milliyetci 10", 3, MilliyetciDislikes, "Images/milliyetci"));
+        cards.Add(new Card(11, "Milliyetci 11", 3, MilliyetciDislikes, "Images/milliyetci"));
+        cards.Add(new Card(12, "Geek 12",4, GeekDislikes, "Images/feminist"));
+        cards.Add(new Card(13, "Geek 13", 4, GeekDislikes, "Images/feminist"));
+        cards.Add(new Card(14, "Geek 14", 4, GeekDislikes, "Images/feminist"));
+        cards.Add(new Card(15, "Asiri Sanatci 15",5, AsiriSanatciDislikes, "Images/feminist"));
+        cards.Add(new Card(16, "Asiri Sanatci 16", 5, AsiriSanatciDislikes, "Images/feminist"));
+        cards.Add(new Card(17, "Asiri Sanatci 17", 5, AsiriSanatciDislikes, "Images/feminist"));
+        cards.Add(new Card(18, "Insel 18",6, InselDislikes, "Images/feminist"));
+        cards.Add(new Card(19, "Insel 19", 6, InselDislikes, "Images/feminist"));
+        cards.Add(new Card(20, "Insel 20", 6, InselDislikes, "Images/feminist"));
+        cards.Add(new Card(21, "Sekuler 21",7, SekulerDislikes, "Images/feminist"));
+        cards.Add(new Card(22, "Sekuler 22", 7, SekulerDislikes, "Images/feminist"));
+        cards.Add(new Card(23, "Sekuler 23", 7, SekulerDislikes, "Images/feminist"));
+        cards.Add(new Card(24, "Dunya Bariscilari 24",8, DunyaBariscisiDislikes, "Images/feminist"));
+        cards.Add(new Card(25, "Dunya Bariscilari 25", 8, DunyaBariscisiDislikes, "Images/feminist"));
+        cards.Add(new Card(26, "Dunya Bariscilari 26", 8, DunyaBariscisiDislikes, "Images/feminist"));
+        cards.Add(new Card(27, "Satanist 27",9, SatanistDislikes, "Images/feminist"));
+        cards.Add(new Card(28, "Satanist 28", 9, SatanistDislikes, "Images/feminist"));
+        cards.Add(new Card(29, "Satanist 29", 9, SatanistDislikes, "Images/feminist"));
          
     }
     
